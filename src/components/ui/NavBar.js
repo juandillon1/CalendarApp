fca1324810352b8f59782692a1a0df2364b5f6d5
Juancito <juancruzdillon123@gmail.com>
2021-08-06 19:04:13 -0300

    Seccion 26 terminada

--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { startLogOut } from '../../actions/auth';
 
 export const NavBar = () => {
+    const {name} = useSelector(state => state.auth);
+    const dispatch = useDispatch();
+    const handleLogOut = () => {
+        dispatch(startLogOut());
+    }
     return (
         <div className="navbar navbar-dark bg-dark mb-4">
             <span className="navbar-brand">
-                Jose
+                {name.charAt(0).toUpperCase() + name.slice(1)}
             </span>
-            <button className="btn btn-outline-danger">
+            <button className="btn btn-outline-danger" onClick={handleLogOut}>
                 <i className="fas fa-sign-out-alt p-1"></i>
                 <span>Salir</span>
             </button>
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,20 +1,33 @@
 import React from 'react';
-import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {HashRouter, Redirect, Switch} from 'react-router-dom';
+import { startChecking } from '../actions/auth';
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { CalendarScreen } from '../components/calendar/CalendarScreen';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
+    const dispatch = useDispatch();
+    const { checking, uid } = useSelector(state => state.auth)
+    useEffect(() => {
+        dispatch(startChecking());
+    }, [dispatch]);
+    if( checking ){
+        return (<i className="fas fa-sync fa-spin fa-5x" style={{position: 'absolute', left: '50%'}}></i>);
+    }
     return (
         <>
-            <Router>
+            <HashRouter>
                 <div>
                     <Switch>
-                        <Route exact path="/login" component={LoginScreen}/>
-                        <Route exact path="/" component={CalendarScreen}/>
+                        <PublicRoute exact path="/login" component={LoginScreen} isAuthenticated={!!uid}/>
+                        <PrivateRoute exact path="/" component={CalendarScreen} isAuthenticated={!!uid}/>
                         <Redirect to="/"/>
                     </Switch>
                 </div>
-            </Router>
+            </HashRouter>
         </>
     )
 }
